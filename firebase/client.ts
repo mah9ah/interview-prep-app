25874fb2f58270277e38b4c57c37a59aa3a0ac34
Sediq <mah9ah@virginia.edu>
2025-12-10 20:13:12 -0500

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDI3al_So47Wtjgr6rXA32GprFnoA28Bjs",
-  authDomain: "preppath-b8c49.firebaseapp.com",
-  projectId: "preppath-b8c49",
-  storageBucket: "preppath-b8c49.firebasestorage.app",
-  messagingSenderId: "119903498009",
-  appId: "1:119903498009:web:b478fb33c7f729b9979f88",
-  measurementId: "G-98RTTP6Y01"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
